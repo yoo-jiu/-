6cfac4f853011a50658f1752466e4b7405c9bf71
yoo-jiu <chosun1275@gmail.com>
2025-11-05 09:22:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 st.title('나의 첫 웹 사이트 만들기!')
 a=st.text_input('안녕,니 이름이 뭐야?')
-st.selectbox('좋아하는 음악 있어?',['K-pop','J-pop','rock','발라드'])
+b=st.selectbox('좋아하는 음악 있어?',['K-pop','J-pop','rock','발라드'])
 if st.button('인사말 생성'):
   st.write(a+' 그게 니 이름이구나! 만나서 반가워!')
+  st.write(b+'를 좋아하는구나! 나도 그거 좋아해!')
