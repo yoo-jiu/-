626dcf689d599a5410d91060cdff23bac895c2fa
yoo-jiu <chosun1275@gmail.com>
2025-11-05 09:13:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.title('나의 첫 웹 사이트 만들기!')
 a=st.text_input('안녕,니 이름이 뭐야?')
 if st.button('인사말 생성'):
-  st.write(a+'그게 니 이름이구나! 만나서 반가워!')
+  st.write(a+' 그게 니 이름이구나! 만나서 반가워!')
